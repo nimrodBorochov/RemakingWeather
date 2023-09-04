51e0457c5500311f54c89a361c87ef4a546a4731
nimrodBorochov <nimrodBorochov@gmail.com>
2023-09-04 16:01:39 +0300

    init project

---
+++ RemakingWeather/RemakingWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemakingWeatherApp.swift
+//  RemakingWeather
+//
+//  Created by nimrod borochov on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RemakingWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
